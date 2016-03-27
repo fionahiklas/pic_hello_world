4b18e046332b658fa0596a358685d603153630f9
Fiona Bianchi <fiona@hiklas.co.uk>
2016-03-27 16:23:09 +0100

    Changed to remove the watchdog timer.
    Still getting the odd pause during flashing of the LEDs.  Added 100nF capacitor across Vdd and Vss pins

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 
 // CONFIG
 #pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTOSC oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
-#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config WDTE = OFF        // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
 #pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
 #pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
