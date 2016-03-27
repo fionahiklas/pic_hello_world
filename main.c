4e2cdb0dd678d3a8e896ddb9599e40c098d526e5
Fiona Bianchi <fiona@hiklas.co.uk>
2016-03-27 22:13:02 +0100

    After much mucking around this code seems right.
    The problem is that the breadboard setup seems to be very susceptible to noise and moving my hand near to it causes strange behaviour of the device.  Need a better circuit board to test things out.  Also need to check if capacitors are needed on MCLR for operation

--- main.c
+++ main.c
@@ -11,14 +11,13 @@
 
 // CONFIG
 #pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTOSC oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
-#pragma config WDTE = OFF        // Watchdog Timer Enable bit (WDT disabled)
+#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
 #pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
 #pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
 #pragma config LVP = ON         // Low-Voltage Programming Enable bit (RB4/PGM pin has PGM function, low-voltage programming enabled)
 #pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
-
 
 void delay(int delay) {
     while(--delay) {
@@ -27,15 +26,17 @@ void delay(int delay) {
 }
 
 void main(void) {
+    // Set clock to 4MHz
+    PCONbits.OSCF = 1;
     TRISA0 = 0;
     TRISA1 = 0;
         
     while(1) {
-        PORTA = 0x03;
+        PORTA = 0x01;
                 
         delay(10000);
         
-        PORTA = 0x00;
+        PORTA = 0x02;
         
         delay(10000);
     }
